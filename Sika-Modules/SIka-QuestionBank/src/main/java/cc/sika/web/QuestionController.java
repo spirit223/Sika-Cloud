6824de38d88bead828b675dc661e2440f41c9d86
spirit223 <2235569493@qq.com>
2022-12-15 21:56:22 +0800

    增加接口声明, 修改返回值

--- Sika-Modules/SIka-QuestionBank/src/main/java/cc/sika/web/QuestionController.java
+++ Sika-Modules/SIka-QuestionBank/src/main/java/cc/sika/web/QuestionController.java
@@ -1,10 +1,17 @@
 package cc.sika.web;
 
-import cc.sika.api.common.HttpStatus;
+import cc.sika.api.domain.BaseResponse;
 import cc.sika.api.domain.Question;
 import cc.sika.api.domain.Topic;
-import cc.sika.api.vo.QuestionVO;
+import cc.sika.api.vo.QuestionDTO;
+import cc.sika.exception.NoQuestionNumberException;
+import cc.sika.exception.WriteQuestionFailException;
 import cc.sika.service.QuestionService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -14,45 +21,93 @@ import java.util.List;
  * <p>
  * 问题相关操作接口
  * </p>
- * todo Swagger配置
  *
  * @author 吴畅
  * @创建时间 2022/12/7 - 16:31
  */
 @RestController
 @RequestMapping("/question")
-public class QuestionController {
+@Slf4j
+public class QuestionController extends BaseController {
     @Resource(name = "questionService")
     private QuestionService service;
 
+
     @GetMapping("/get/{id}")
-    public QuestionVO getQueById(@PathVariable("id") int questionId) {
-        return service.getQuesById(questionId);
+    @ApiOperation(value = "获取题目", notes = "<span style='color: red'>通过id来获获取题目信息</span>")
+    @ApiImplicitParams(@ApiImplicitParam(name = "questionId", dataTypeClass = Integer.class, value = "要获取信息的题目的id"))
+    @ApiResponse(code = 200, message = "返回BaseResponse对象, 对象信息存储在data域中")
+    public BaseResponse<QuestionDTO> getQueById(@PathVariable("id") int questionId) throws NoQuestionNumberException {
+        return responseSuccess(service.getQuesById(questionId));
     }
 
     @GetMapping("/topic/{topic}")
-    public List<QuestionVO> getQueByTopic(@PathVariable("topic") Topic topic) {
-        return service.getQuesByTopic(topic);
+    @ApiOperation(value = "获取多个题目", notes = "<span style='color: red'>通过分类来获取题目信息</span>")
+    @ApiImplicitParams(@ApiImplicitParam(name = "topic", dataTypeClass = Topic.class, value = "要获取信息的题目的分类"))
+    @ApiResponse(code = 200, message = "返回BaseResponse对象, data域中为 符合分类的题目信息列表")
+    public BaseResponse<List<QuestionDTO>> getQueByTopic(@PathVariable("topic") Topic topic) {
+        return responseSuccess(service.getQuesByTopic(topic));
     }
 
     @GetMapping("/get/all")
-    public List<QuestionVO> getAllQues() {
-        return service.getAllQues();
+    @ApiOperation(value = "获取所有题目")
+    @ApiResponse(code = 200, message = "返回BaseResponse对象, data域中为 所有题目信息列表")
+    public BaseResponse<List<QuestionDTO>> getAllQues() {
+        return responseSuccess(service.getAllQues());
     }
 
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * 通过 POST 方式添加一个问题
+     *
+     * @param question 获取到请求体中的整个 Question 对象写入数据库
+     * @return 返回写入的结果
+     */
     @PostMapping("/add")
-    public HttpStatus addQue(@RequestBody Question question) {
-        return service.addQuestion(question);
+    @ApiOperation(value = "通过 POST 方式添加题目")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "question",
+                    dataTypeClass = Question.class,
+                    value = "要添加的题目对象",
+                    required = true)
+    )
+    @SuppressWarnings("rawtypes")
+    public BaseResponse addQue(@RequestBody Question question) throws WriteQuestionFailException {
+        return responseSuccess(service.addQuestion(question), "添加题目成功");
     }
 
+    /**
+     * 通过 PUT 方式更新问题
+     *
+     * @param question 获取到请求体中的整个 Question 对象覆盖写入数据库
+     * @return 返回写入的结果
+     */
     @PutMapping("/update")
-    public HttpStatus updateQue(@RequestBody Question question) {
-        return service.updateQuestion(question);
+    @ApiOperation(value = "通过 PUT 方式更新题目")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "question",
+                    dataTypeClass = Question.class,
+                    value = "要更新的题目对象, 通过id来确定, 如果更新的id值未存在, 则不会有任何操作",
+                    required = true)
+    )
+    @SuppressWarnings("rawtypes")
+    public BaseResponse updateQue(@RequestBody Question question) throws WriteQuestionFailException {
+        return responseSuccess(service.updateQuestion(question));
     }
 
+    /**
+     * 通过问题 id, 使用 DELETE 请求方式删除问题
+     *
+     * @param questionId 要删除的问题id
+     * @return 删除结果
+     */
     @DeleteMapping("/delete/{id}")
-    public HttpStatus deleteQue(@PathVariable("id") int questionId) {
-        return service.deleteQuestionById(questionId);
+    @ApiOperation(value = "删除题目", notes = "<span style='color: red'>通过id来删除题目</span>")
+    @ApiImplicitParams(@ApiImplicitParam(name = "questionId", dataTypeClass = Integer.class, value = "要删除信息的题目id"))
+    @SuppressWarnings("rawtypes")
+    public BaseResponse deleteQue(@PathVariable("id") int questionId) throws WriteQuestionFailException {
+        return responseSuccess(service.deleteQuestionById(questionId));
     }
 
 }
