cd3c69a18113bb72ba2ea163fdb539aa52569b64
spirit223 <2235569493@qq.com>
2022-12-15 21:53:20 +0800

    业务代码逻辑修改和异常声明

--- Sika-Modules/SIka-QuestionBank/src/main/java/cc/sika/service/impl/QuestionServiceImpl.java
+++ Sika-Modules/SIka-QuestionBank/src/main/java/cc/sika/service/impl/QuestionServiceImpl.java
@@ -3,7 +3,9 @@ package cc.sika.service.impl;
 import cc.sika.api.common.HttpStatus;
 import cc.sika.api.domain.Question;
 import cc.sika.api.domain.Topic;
-import cc.sika.api.vo.QuestionVO;
+import cc.sika.api.vo.QuestionDTO;
+import cc.sika.exception.NoQuestionNumberException;
+import cc.sika.exception.WriteQuestionFailException;
 import cc.sika.mapper.AnswerMapper;
 import cc.sika.mapper.QuestionMapper;
 import cc.sika.service.QuestionService;
@@ -27,24 +29,25 @@ public class QuestionServiceImpl implements QuestionService {
     /**
      * 通过 id 获取 QuestionVO 对象, 对象中携带 answer 属性
      *
-     * @param questionId
      * @return 带有answer属性的QuestionVO
      */
     @Override
-    public QuestionVO getQuesById(int questionId) {
+    public QuestionDTO getQuesById(int questionId) throws NoQuestionNumberException {
         Question question = questionMapper.queryQuestionById(questionId);
+        if (question == null) {
+            throw new NoQuestionNumberException("There is no question with number: " + questionId);
+        }
         String answerContent = answerMapper.queryContentByQueId(question.getQuestionId());
-        return new QuestionVO(question, answerContent);
+        return new QuestionDTO(question, answerContent);
     }
 
     /**
      * 通过题目的 topic 获取一个带有answer属性的QuestionVO 列表
      *
-     * @param topic
      * @return 带有answer属性的QuestionVO-List<QuestionVO>
      */
     @Override
-    public List<QuestionVO> getQuesByTopic(Topic topic) {
+    public List<QuestionDTO> getQuesByTopic(Topic topic) {
         List<Question> questions = questionMapper.queryQuestionByTopic(topic);
         return toVO(questions);
     }
@@ -55,36 +58,39 @@ public class QuestionServiceImpl implements QuestionService {
      * @return 带有answer属性的QuestionVO-List
      */
     @Override
-    public List<QuestionVO> getAllQues() {
+    public List<QuestionDTO> getAllQues() {
         List<Question> questions = questionMapper.queryAllQuestion();
         return toVO(questions);
     }
 
     @Override
-    public HttpStatus addQuestion(Question question) {
+    public HttpStatus addQuestion(Question question) throws WriteQuestionFailException {
         int addResult = questionMapper.addQuestion(question);
         if (addResult > 0) {
             return HttpStatus.SUCCESS;
+        } else {
+            throw new WriteQuestionFailException("添加题目失败!");
         }
-        return HttpStatus.ERROR;
     }
 
     @Override
-    public HttpStatus updateQuestion(Question question) {
+    public HttpStatus updateQuestion(Question question) throws WriteQuestionFailException {
         int updateResult = questionMapper.updateQuestion(question);
         if (updateResult > 0) {
             return HttpStatus.SUCCESS;
+        } else {
+            throw new WriteQuestionFailException("修改题目失败!");
         }
-        return HttpStatus.ERROR;
     }
 
     @Override
-    public HttpStatus deleteQuestionById(int questionId) {
+    public HttpStatus deleteQuestionById(int questionId) throws WriteQuestionFailException {
         int deleteResult = questionMapper.deleteQuestion(questionId);
         if (deleteResult > 0) {
             return HttpStatus.SUCCESS;
+        } else {
+            throw new WriteQuestionFailException("删除题目失败!");
         }
-        return HttpStatus.ERROR;
     }
 
     /**
@@ -93,10 +99,10 @@ public class QuestionServiceImpl implements QuestionService {
      * @param questionList 没有answer属性的Question的List<Question>
      * @return 带有answer属性的QuestionVO的List<QuestionVO>
      */
-    private List<QuestionVO> toVO(List<Question> questionList) {
+    private List<QuestionDTO> toVO(List<Question> questionList) {
         return questionList.stream().map(question -> {
             String answer = answerMapper.queryContentByQueId(question.getQuestionId());
-            return new QuestionVO(question, answer);
+            return new QuestionDTO(question, answer);
         }).collect(Collectors.toList());
     }
 }
