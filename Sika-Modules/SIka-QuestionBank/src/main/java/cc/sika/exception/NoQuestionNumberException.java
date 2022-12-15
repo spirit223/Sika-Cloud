ed829cc7580cb80463070a3ab5a28bb7e70a163e
spirit223 <2235569493@qq.com>
2022-12-15 21:51:47 +0800

    读写题目相关异常

---
+++ Sika-Modules/SIka-QuestionBank/src/main/java/cc/sika/exception/NoQuestionNumberException.java
@@ -0,0 +1,12 @@
+package cc.sika.exception;
+
+/**
+ * @author 吴畅
+ * @创建时间 2022/12/15 - 16:43
+ */
+public class NoQuestionNumberException extends Exception {
+
+    public NoQuestionNumberException(String message) {
+        super(message);
+    }
+}
---
+++ Sika-Modules/SIka-QuestionBank/src/main/java/cc/sika/exception/WriteQuestionFailException.java
@@ -0,0 +1,11 @@
+package cc.sika.exception;
+
+/**
+ * @author 吴畅
+ * @创建时间 2022/12/15 - 21:34
+ */
+public class WriteQuestionFailException extends Exception {
+    public WriteQuestionFailException(String message) {
+        super(message);
+    }
+}
